bd0b9a8878b9754405a7a5d46f76c81e1408f42f
DanielMalede <danielmalede@gmail.com>
2022-06-06 14:34:21 +0300

    .

--- main.js
+++ main.js
@@ -8,27 +8,33 @@
     document.body.append(userAge)
     userAge.id= "userAge"
     userAge.type="date"
-    userAge.placeholder="enter your Age"
-
+    
+    
     var myButton = document.createElement("button")
     document.body.append(myButton)
     myButton.innerText="send" 
     myButton.addEventListener("click",chackUserAge)
-
- function chackUserAge() {
-     var thisDay = new Date()
-     var h3_line= document.createElement("h3")
-     document.body.append(h3_line)
-     if (thisDay.getFullYear-userAge.value>18) {
-        h3_line.style.innerText = "Welcome"+nameinput.value
-        h3_line.style="color:blue"
-     }
-     else{
-         h3_line.innerText="No Entry!"+"<img src="+"https://i.gifer.com/7rO1.gif"+">"
-         h3_line.style="color:red"
-     }   
-}
-
-
-
-
+    
+    var thisDay = new Date()
+    
+    function turntonumber(str) {
+        for (var i = 0; i < str.lenght; i++) {
+            var year= ""
+            year+=str[i]
+            if (str[i]=="-") {
+                return parseInt (year)             
+            }   
+        }
+    }
+    function chackUserAge() {
+        var h3_line= document.createElement("h3")
+        document.body.append(h3_line)
+        if (thisDay.getFullYear()- turntonumber(userAge.value)<18) {
+            h3_line.innerHTML="No Entry!"+"<br>"+"<img src=https://i.gifer.com/7rO1.gif>";
+            h3_line.style.color="red"
+        }
+        else{
+            h3_line.style.innerText = "Welcome"+nameinput.value
+            h3_line.style="color:blue"
+        }   
+    }

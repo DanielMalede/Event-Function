71f25e1864a3ccfb3231b861427d1527ff686306
DanielMalede <danielmalede@gmail.com>
2022-06-07 13:42:22 +0300

    .

--- main.js
+++ main.js
@@ -18,55 +18,89 @@ var h3_line= document.createElement("h3");
 document.body.append(h3_line);
 
 h3_line.addEventListener("mouseover",()=>{
-    h3_line.style.color="green"; });
+h3_line.style.color="green"; });
 
 var thisDay = new Date();
 
 var isEighteen=false;
 function chackUserAge() {
-    var userYear = +userAge.value.substring(0,4);
-    if (thisDay.getFullYear()-userYear >18) {
-        h3_line.innerText = "Welcome"+nameinput.value;
-        h3_line.style="color:blue";
-        isEighteen=true;
-    }
-    else{
-        h3_line.innerHTML="No Entry!"+"<br>"+"<img src=https://i.gifer.com/7rO1.gif>";
-        h3_line.style.color="red";         
-    }   
+var userYear = +userAge.value.substring(0,4);
+if (thisDay.getFullYear()-userYear >18) {
+    h3_line.innerText = "Welcome"+nameinput.value;
+    h3_line.style="color:blue";
+    isEighteen=true;
+}
+else{
+    h3_line.innerHTML="No Entry!"+"<br>"+"<img src=https://i.gifer.com/7rO1.gif>";
+    h3_line.style.color="red";         
+}   
 
-    if(isEighteen){
-        var array = []
+if(isEighteen){
+    var array = []
 
-        var myList = document.createElement("ul")
-        
-        var userLastName = document.createElement("input");
-        document.body.append(userLastName);
-        userLastName.placeholder="enter your last name";
-        
-        var inputUserName = document.createElement("input");
-        document.body.append(inputUserName);
-        inputUserName.placeholder="enter your name";
-        
-        var arrayButton = document.createElement("button");
-        document.body.append(arrayButton);
-        arrayButton.innerText="clack me"
-        arrayButton.addEventListener("click",()=>{
-            array.push(inputUserName.value)
-            inputUserName.value=""
-            if (array.length==4) {
-                for (var i = 0; i < array.length; i++) {
-                       myList.innerHTML+="<li id=list"+i+">"+array[i]+" " +userLastName.value+"</li>"       
-                       var myarray = array[i]
-                       if (myarray[0]==userLastName.value[0]) {
-                        document.getElementById("list"+i).style="color:blue";
-                    }
+    var myList = document.createElement("ul")
+    
+    var userLastName = document.createElement("input");
+    document.body.append(userLastName);
+    userLastName.placeholder="enter your last name";
+    
+    var inputUserName = document.createElement("input");
+    document.body.append(inputUserName);
+    inputUserName.placeholder="enter your name";
+    
+    var arrayButton = document.createElement("button");
+    document.body.append(arrayButton);
+    arrayButton.innerText="clack me"
+    arrayButton.addEventListener("click",()=>{
+        array.push(inputUserName.value)
+        inputUserName.value=""
+        if (array.length==4) {
+            for (var i = 0; i < array.length; i++) {
+                    myList.innerHTML+="<li id=list"+i+">"+array[i]+" " +userLastName.value+"</li>"       
+                    var myarray = array[i]
+                    if (myarray[0]==userLastName.value[0]) {
+                    document.getElementById("list"+i).style="color:blue";
                 }
             }
-            document.body.append(myList);
-        })
-    }
+        }
+        document.body.append(myList);
+    })
 }
+}
+
+
+function creatinput() {
+    var search_som = document.createElement("input")
+    search_som.type("search")
+    document.body.append(search_som)
+}
+
+
+var newbutton = document.createElement("button")
+newbutton.innerText="clack me"
+document.body.append(newbutton)
+newbutton.addEventListener("click",creatinput)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
